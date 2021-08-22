a9025c62d504ea938326e71c104ce8eb05208529
Tony Medel <antonio.medel2021@email.bakersfieldcollege.edu>
2021-08-22 02:20:36 -0700

    feat(add-guard-state): state control in login & add guarded route to router

--- application/src/router/appRouter.js
+++ application/src/router/appRouter.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Main, Login, OrderForm, ViewOrders } from '../components';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Main, Login, OrderForm, ViewOrders } from "../components";
+import GuardedRoute from "./guardedRoute";
 
 const AppRouter = (props) => {
-  return (
-    <Router>
-      <Route path="/" exact component={Main} />
-      <Route path="/login" exact component={Login} />
-      <Route path="/order" exact component={OrderForm} />
-      <Route path="/view-orders" exact component={ViewOrders} />
-    </Router>
-  );
-}
+	return (
+		<Router>
+			<Route path="/" exact component={Main} />
+			<Route path="/login" exact component={Login} />
+			<GuardedRoute path="/order" exact component={OrderForm} />
+			<GuardedRoute path="/view-orders" exact component={ViewOrders} />
+		</Router>
+	);
+};
 
 export default AppRouter;
