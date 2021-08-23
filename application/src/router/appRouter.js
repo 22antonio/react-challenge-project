fa354121d7265da1d4b4cea27b5b657c7aea15a5
Tony Medel <antonio.medel2021@email.bakersfieldcollege.edu>
2021-08-23 01:06:28 -0700

    feat(add-edit): edits are a new page, add route to router

--- application/src/router/appRouter.js
+++ application/src/router/appRouter.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Main, Login, OrderForm, ViewOrders } from '../components';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Main, Login, OrderForm, ViewOrders } from "../components";
+import EditOrder from "../components/edit-form/editOrder";
 
 const AppRouter = (props) => {
-  return (
-    <Router>
-      <Route path="/" exact component={Main} />
-      <Route path="/login" exact component={Login} />
-      <Route path="/order" exact component={OrderForm} />
-      <Route path="/view-orders" exact component={ViewOrders} />
-    </Router>
-  );
-}
+	return (
+		<Router>
+			<Route path="/" exact component={Main} />
+			<Route path="/login" exact component={Login} />
+			<Route path="/order" exact component={OrderForm} />
+			<Route path="/view-orders" exact component={ViewOrders} />
+			<Route path="/edit-order" exact component={EditOrder} />
+		</Router>
+	);
+};
 
 export default AppRouter;
